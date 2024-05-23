9e2cbecaa9c8f4475f735f3337b0e496a35b3e27
Xenia Gontar <118064189+xenia-gontar@users.noreply.github.com>
2024-05-23 16:39:15 +0300

    Update tasks_get.py

--- tasks_get.py
+++ tasks_get.py
@@ -1,10 +1,24 @@
 from bitrix24 import Bitrix24
 
+
 bx24 = Bitrix24('webhook')
 
-# Получение id задач по проекту
-print(bx24.callMethod("tasks.task.list",
-    filter={'GROUP_ID': 1},
-	select=['ID']))
 
-# Получение записей в задаче по учету времени за сегодня
+
+# Получение id задач по проекту
+
+response = bx24.callMethod("tasks.task.list",
+    filter={'GROUP_ID': 1},
+	select=['ID'])
+
+print(response)
+print(response["tasks"])
+
+# Достали все id задач
+
+tasks_id = []
+
+for element in response["tasks"]:
+	tasks_id.append(element["id"])
+
+print(tasks_id)	
